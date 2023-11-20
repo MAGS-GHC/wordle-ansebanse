e7a594cb93b3f89b25f0911172524703ee39aff7
ansebanse <146828882+ansebanse@users.noreply.github.com>
2023-11-20 14:07:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
-// Creating the squares:
+// Creating the squares: 
 document.addEventListener("DOMContentLoaded", () => {
     createSquares();
 
-  function createSquares() {
-    const gameBoard = document.getElementById("board");
+    let GuessedWords = [[]];
+    let availableSpace = 1;
+
+    let word = "Dairy"
+
+    const keys = document.querySelectorAll('.keyboard-row button');
+
+    function HandleSubmitWord() {
+
+    }
+
+
+    function getCurrentWorldArray() {
+        const numberOfGuessedWords = GuessedWords.length
+        return GuessedWords[numberOfGuessedWords - 1];
+    }
+
+    
+    function UpdateGuessedWords(letter) {
+        const CurrentWorldArray = getCurrentWorldArray();
+
+        if(CurrentWorldArray && CurrentWorldArray.length < 5) {
+            CurrentWorldArray.push(letter)
+
+            const availableSpaceEl = document.getElementById(String(availableSpace));
+            availableSpace = availableSpace + 1;
+
+            availableSpaceEl.textContent = letter;
+        }
+
+    }
+
+    function createSquares(){
+        const gameBoard = document.getElementById("board");
 
         for(let index = 0; index < 30; index++) {
             let square = document.createElement("div");
@@ -12,6 +44,17 @@ document.addEventListener("DOMContentLoaded", () => {
             gameBoard.appendChild(square);
         }
     }
-});
 
-//hey Anna
+    for (let i = 0; i < keys.length; i++) {
+        keys[i].onclick = ({ target }) => {
+            const letter = target.getAttribute("data-key")
+
+            UpdateGuessedWords(letter);
+
+            if(letter === 'Enter') {
+                HandleSubmitWord()
+            }
+        }; 
+    }
+
+});
